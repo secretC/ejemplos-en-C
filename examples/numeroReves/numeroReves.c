43f69b0c930eac90c48c05ae1696d5af0ef5f5ba
Franz Victorio <evenhold@gmail.com>
2016-11-12 19:17:07 -0500

    README.md updated

--- examples/numeroReves/numeroReves.c
+++ examples/numeroReves/numeroReves.c
@@ -10,34 +10,33 @@
 
  Procedimiento:
  	
- 	-	Entonces:
-	 		123 % 10 = 3; --> Capturamos este número.
-	 		123 - 3  = 120;
-	 		120 / 10 = 12;
-			-------------------
-			12  % 10 = 2; --> Capturamos este número.
-			12  - 2  = 10;
-			10  / 10 = 1; 
-			-------------------
-			1   % 10 = 1; --> Capturamos este número.
-			1   - 1  = 0;
-			0   / 10 = 0; 
-			------------------- 
-			0   % 10 = 0 --> Valor que no se debe mostrar.
+ 		123 % 10 = 3; --> Capturamos este número.
+ 		123 - 3  = 120;
+ 		120 / 10 = 12;
+		-------------------
+		12  % 10 = 2; --> Capturamos este número.
+		12  - 2  = 10;
+		10  / 10 = 1; 
+		-------------------
+		1   % 10 = 1; --> Capturamos este número.
+		1   - 1  = 0;
+		0   / 10 = 0; 
+		------------------- 
+		0   % 10 = 0 --> Valor que no se debe mostrar.
 
 
 
-			---> "0" no se debe imprimir en la terminal,seria la condición, 
-			cuando el nuevo valor de "numero" es igual a  cero la repetición termina(bucle).
+		---> "0" no se debe imprimir en la terminal,seria la condición, 
+		cuando el nuevo valor de "numero" es igual a  cero la repetición termina(bucle).
 
-			De aqui no fijamos dos cosas:
-				- 10 es constante.
-				- La repitición termina cuando el nuevo valor de "numero" es cero.
- 		
-		- Function:
-				resto = numero % 10;
-				resta = numero - resto;
-				numero = resta /10;  >>> Es el nuevo valor de "numero"
+		De aqui no fijamos dos cosas:
+			- 10 es constante.
+			- La repitición termina cuando el nuevo valor de "numero" es cero.
+		
+	- Function:
+			resto = numero % 10;
+			resta = numero - resto;
+			numero = resta /10;  >>> Es el nuevo valor de "numero"
 */
 
 
