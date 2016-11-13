3de599fa3db56a7f06fd380ca8e7484cc8017d01
Franz Victorio <evenhold@gmail.com>
2016-11-12 19:58:16 -0500

    README.md updated

---
+++ examples/rombo/rombo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+void main()
+{
+	int n,x,y,h;
+    printf("n = ");
+    scanf("%d",&n);
+    h = (n-1)/2;
+    for (y=-h; y <= h ; y++)
+    {
+        for (x=h; x >= -h; x--)
+            if ( (abs(x) + abs(y)) <= h ) printf("*"); else printf(" ");
+        printf("\n");
+    }
+}
