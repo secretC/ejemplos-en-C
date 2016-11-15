1411ff15e46904615535fdbab5288f96e0fa5d30
Franz Victorio <evenhold@gmail.com>
2016-11-14 21:41:38 -0500

    numeroReves.c updated

---
+++ examples/factorial/factorial.c
@@ -0,0 +1,26 @@
+/*
+ * factorial.c.
+ * Author: Franz Victorio
+ * Description: Factorial de un número.
+ * Date: 14/11/16
+ */
+
+#include <stdio.h>
+// Función recursiva.
+int factorial(int n){
+	int resultado;
+	if(n <= 1){
+		return 1;
+	}else{
+		resultado = n * factorial(n - 1);
+	}
+	return resultado;
+}
+int main(){
+	int numero;
+	puts("Ingresar número: ");
+	scanf("%d", &numero);
+	printf("Factorial de %d es: %d\n", numero, factorial(numero));
+	return 0;
+}
+
